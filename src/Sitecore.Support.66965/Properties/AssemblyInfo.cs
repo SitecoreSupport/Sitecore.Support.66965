d54d42af2ef6f766e4c34a21b2072610b5491194
DK\psk <psk@sitecore.net>
2018-06-01 13:10:35 +0300

    Init

---
+++ src/Sitecore.Support.66965/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.66965")]
+[assembly: AssemblyProduct("Sitecore.Support.66965")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Export form data to Excel operation is slow")]
+
