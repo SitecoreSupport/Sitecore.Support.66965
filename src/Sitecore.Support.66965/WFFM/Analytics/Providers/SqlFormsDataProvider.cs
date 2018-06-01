7498d50e815c132fb9ed3ab64104556de9346e97
DK\psk <psk@sitecore.net>
2018-06-01 23:25:57 +0300

    Optimize query

--- src/Sitecore.Support.66965/WFFM/Analytics/Providers/SqlFormsDataProvider.cs
+++ src/Sitecore.Support.66965/WFFM/Analytics/Providers/SqlFormsDataProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using Sitecore.WFFM.Abstractions.Analytics;
 using Sitecore.WFFM.Abstractions.Shared;
 
 namespace Sitecore.Support.WFFM.Analytics.Providers
@@ -15,17 +16,18 @@ namespace Sitecore.Support.WFFM.Analytics.Providers
     {
       this._settings = settings;
       this._connectionProvider = connectionProvider;
-      this._connectionString = connectionStringName;
+      this._connectionString = settings.GetConnectionString(connectionStringName);
     }
 
-    public override IEnumerable<Sitecore.WFFM.Abstractions.Analytics.FormData> GetFormData(Guid formId)
+    public override IEnumerable<FormData> GetFormData(Guid formId)
     {
       if (this._settings.IsRemoteActions)
       {
-        return new List<Sitecore.WFFM.Abstractions.Analytics.FormData>();
+        return new List<FormData>();
       }
 
-      var formDataList = new List<Sitecore.WFFM.Abstractions.Analytics.FormData>();
+      var formDataList = new Dictionary<Guid, FormData>();
+      var fieldDataList = new Dictionary<Guid, List<FieldData>>();
       bool isError = false;
       using (var sql = this._connectionProvider.GetConnection(this._connectionString))
       {
@@ -33,7 +35,7 @@ namespace Sitecore.Support.WFFM.Analytics.Providers
         using (var sqlCommand = sql.CreateCommand())
         {
           sqlCommand.Connection = sql;
-          sqlCommand.CommandText = string.Format("SELECT [Id],[FormItemId],[ContactId],[InteractionId],[TimeStamp],[Data] FROM [FormData] WHERE [FormItemId]=@p1");
+          sqlCommand.CommandText = string.Format("SELECT [FormData].[Id] as FormDataId,[ContactId],[InteractionId],[TimeStamp],[FieldData].[Id] as FieldDataId,[FieldItemId],[FieldName],[Value],[FieldData].[Data] as [Data] FROM [FormData] INNER JOIN [FieldData] ON [FormData].Id=[FieldData].FormId AND [FormItemId]=@p1");
           sqlCommand.Parameters.Add(new SqlParameter("p1", formId));
           sqlCommand.CommandType = CommandType.Text;
           var reader = sqlCommand.ExecuteReader();
@@ -41,15 +43,32 @@ namespace Sitecore.Support.WFFM.Analytics.Providers
           {
             while (reader.Read())
             {
-              var formData = new Sitecore.WFFM.Abstractions.Analytics.FormData
+              var formData = new FormData
               {
                 Id = reader.GetGuid(0),
-                FormID = reader.GetGuid(1),
-                ContactId = reader.GetGuid(2),
-                InteractionId = reader.GetGuid(3),
-                Timestamp = reader.GetDateTime(4)
+                FormID = formId,
+                ContactId = reader.GetGuid(1),
+                InteractionId = reader.GetGuid(2),
+                Timestamp = reader.GetDateTime(3)
               };
-              formDataList.Add(formData);
+              if (!formDataList.ContainsKey(formData.Id))
+              {
+                formDataList[formData.Id] = formData;
+              }
+              var fieldData = new FieldData
+              {
+                Id = reader.GetGuid(4),
+                FieldId = reader.GetGuid(5),
+                FieldName = reader["FieldName"] as string,
+                Form = formData,
+                Value = reader["Value"] as string,
+                Data = reader["Data"] as string
+              };
+              if (!fieldDataList.ContainsKey(formData.Id))
+              {
+                fieldDataList[formData.Id] = new List<FieldData>();
+              }
+              fieldDataList[formData.Id].Add(fieldData);
             }
           }
           catch
@@ -63,56 +82,16 @@ namespace Sitecore.Support.WFFM.Analytics.Providers
         }
       }
 
-      if (!isError && formDataList.Count > 0)
+      foreach (var formData in formDataList)
       {
-        foreach (var formData in formDataList)
+        var fieldList = fieldDataList[formData.Key];
+        if (fieldList.Count > 0)
         {
-          var fieldList = new List<Sitecore.WFFM.Abstractions.Analytics.FieldData>();
-          using (var sql = this._connectionProvider.GetConnection(this._connectionString))
-          {
-            sql.Open();
-            using (var sqlCommand = sql.CreateCommand())
-            {
-              sqlCommand.Connection = sql;
-              sqlCommand.CommandText = string.Format("SELECT [Id],[FieldItemId],[FieldName],[Value],[Data] FROM [FieldData] WHERE [FormId]=@p1");
-              sqlCommand.Parameters.Add(new SqlParameter("p1", formData.Id));
-              sqlCommand.CommandType = CommandType.Text;
-              var reader = sqlCommand.ExecuteReader();
-              try
-              {
-                while (reader.Read())
-                {
-                  var fieldData = new Sitecore.WFFM.Abstractions.Analytics.FieldData
-                  {
-                    Id = new Guid(reader["Id"].ToString()),
-                    FieldId = new Guid(reader["FieldItemId"].ToString()),
-                    FieldName = reader["FieldName"] as string,
-                    Form = formData,
-                    Value = reader["Value"] as string,
-                    Data = reader["Data"] as string
-                  };
-                  fieldList.Add(fieldData);
-                }
-              }
-              catch
-              {
-                isError = true;
-              }
-              finally
-              {
-                reader.Close();
-              }
-            }
-          }
-
-          if (fieldList.Count > 0)
-          {
-            formData.Fields = fieldList;
-          }
+          formData.Value.Fields = fieldList;
         }
       }
 
-      return isError ? new List<Sitecore.WFFM.Abstractions.Analytics.FormData>() : formDataList;
+      return isError ? new List<FormData>() : formDataList.Values as IEnumerable<FormData>;
     }
   }
 }
